c9ed194388cebdf7b1a1794176a5dd1e64842e3d
julius90 <airj@gmx.net>
2013-03-20 13:22:27 +0100

    my header test

---
+++ Future-SATSolver/newfile.hpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   newfile.h
+ * Author: juliushofler
+ *
+ * Created on 20. März 2013, 13:10
+ */
+
+#ifndef NEWFILE_H
+#define	NEWFILE_H
+
+#include  <iostream>
+#include <stdio.h> 
+#include <stdlib.h> 
+
+using namespace std;
+
+class Test {
+
+    public:
+        Test();
+	~Test();
+	void test ();
+
+};
+
+
+
+#endif	/* NEWFILE_H */
+
