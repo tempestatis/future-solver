e083778b5caa44308de1e31ece641f0c417f4e7c
julius90 <airj@gmx.net>
2013-04-05 16:41:03 +0200

    new vnd source version

--- Future-SATSolver/Source/VND.cpp
+++ Future-SATSolver/Source/VND.cpp
@@ -41,11 +41,184 @@ int VND::search(){
         return 1;
     }
     
-    unsigned int numberOfNeighbourhoods = 20;
+    unsigned int numberOfNeighbourhoods = 100;
+    unsigned int initalNumberOfNeighbourhoods = 100;
     unsigned int currentNeighbourhood = 1;
     unsigned int indexOfNeighbourhoods = 0;
     unsigned int flips = 1;
-    unsigned int maxFlips = 20;
+    unsigned int maxFlips = 100;
+    unsigned int rest = 0;
+    unsigned int numberOfTriesInOneFlip = 0;
+    
+    // maximal number of flips
+    if (numberOfVariables < maxFlips) {
+        maxFlips = (int) numberOfVariables;
+    }
+    
+    cout << "First SAT Clauses: " << startNumberOfSatisfiedClauses << endl;
+    
+    // ends if there is a solution
+    while(true) {
+    
+        //+++++++++++++
+        // TODO: wenn er in einem flip abbricht und noch nicht fertig mit
+        //       dem flip ist, dann flip nicht erhöhen
+        //      Note: Anzahl der Nachbarn gleich halten und nur in diesem Bereich springen
+        //      --> creatNeighbour solange aufrufen bis Grenze erreicht ist und dann Grenze erhöhen (Ausschnitt)
+        //+++++++++++++
+        
+        // runs until all variables were flipped in this level or number of neighbourhoods were reached    
+        while( currentNeighbourhood <= numberOfNeighbourhoods ) {
+
+            rest = solvObject->createNeighbour(flips);
+            //numberOfTriesInOneFlip += 1;
+            
+            if (rest == 1) {
+                flips += 1;
+                //cout << "Number: " << numberOfTriesInOneFlip << endl;
+                //numberOfTriesInOneFlip = 0;
+                //cout << "flips: " << flips << endl;
+                solvObject->resetFlipper();
+                solvObject->createNeighbour(flips);
+            } 
+            
+            // shift the index if maxFlip is reached
+            if (maxFlips < flips) {
+
+                    //flips = 1;
+                    //indexOfNeighbourhoods += numberOfNeighbourhoods;
+                    //solvObject->resetFlipper();
+
+                    // automaticly unsat if index is higher than number of variables
+                    //if ( (indexOfNeighbourhoods + 1) > numberOfVariables) {
+                            printf("s UNSATISFIABLE\n");
+                            return 0;
+                    //}
+            }
+
+            // flip values
+            solvObject->flipVariablesByFlipperVector();
+
+            // get new number of satisfied clauses
+            newNumberOfSatisfiedClauses = solvObject->getNumberOfSatisfiedClauses();
+            
+            //cout << "Test: " << newNumberOfSatisfiedClauses << endl;
+
+            // check whether it is better than the old
+            if (newNumberOfSatisfiedClauses > bestNumberOfSatisfiedClauses) {
+                solvObject->copyFlipper(flipperCopy);
+                bestNumberOfSatisfiedClauses = newNumberOfSatisfiedClauses;
+            }
+            
+            // check whether there is a solution
+            if (newNumberOfSatisfiedClauses == numberOfClauses) {
+                printf("s SATISFIABLE\n");
+                return 1;
+            }
+
+            // inc number of sawn neighbours
+            currentNeighbourhood += 1; 
+            
+            
+            //cout << "current SAT clauses: " << newNumberOfSatisfiedClauses << endl;
+            //solvObject->printVariablesAssignment();
+            
+            // revert flip
+            solvObject->flipVariablesByFlipperVector();
+            
+            // just a try
+            //if (numberOfNeighbourhoods > 20000) {
+            //    flips += 1;
+            //    numberOfNeighbourhoods = initalNumberOfNeighbourhoods;
+            //    solvObject->resetFlipper();
+            //}
+            
+            // new random list
+            if (numberOfNeighbourhoods > 2000) {   
+                return 0;
+             
+            }    
+               
+                
+        }
+    
+        cout << "Next best SAT Clauses: " << bestNumberOfSatisfiedClauses << endl;
+        
+        // no improvements with this flip
+        if (startNumberOfSatisfiedClauses >= bestNumberOfSatisfiedClauses) {
+            
+            // gets the best variable combination back
+            //solvObject->useFlipperCopy(flipperCopy);
+            //solvObject->flipVariablesByFlipperVector();
+            
+            //flips += 1;
+            //solvObject->resetFlipper();
+            //cout << "next flip: " << flips << endl;
+            
+            // inc neighbourhood
+            numberOfNeighbourhoods += 50;
+        }
+    
+        // get the best neighbour and continue
+        else {
+            solvObject->useFlipperCopy(flipperCopy);
+            solvObject->flipVariablesByFlipperVector();
+            
+            flips = 1;
+            numberOfTriesInOneFlip = 0;
+            numberOfNeighbourhoods = initalNumberOfNeighbourhoods;
+            startNumberOfSatisfiedClauses = bestNumberOfSatisfiedClauses;
+            solvObject->resetFlipper();
+        }
+    
+        // set back for a new run
+        currentNeighbourhood = 1;
+        
+        cout << "Number of Neighbourhoods: " << numberOfNeighbourhoods << endl;
+        //solvObject->printVariablesAssignment();
+        
+    
+    }
+ 
+    return 0;
+}  
+    
+    
+
+
+
+
+    // inc only flips, not neighbours
+    int VND::search_secondTry(){
+    
+    vector <int>::size_type numberOfVariables;
+    flippercopy flipperCopy;
+    unsigned int newNumberOfSatisfiedClauses;
+    
+    // number of variables
+    numberOfVariables = solvObject->getListOfVariables()->size();
+    
+    // number of clauses
+    unsigned int numberOfClauses = solvObject->getNumberOfClauses();
+    
+    // initial number of satisfied clauses
+    unsigned int startNumberOfSatisfiedClauses = solvObject->getNumberOfSatisfiedClauses();
+    unsigned int bestNumberOfSatisfiedClauses = startNumberOfSatisfiedClauses;
+    
+    // copy initial state in flipper
+    solvObject->initializeCopyFlipper(flipperCopy);
+    
+    // check initial state whether it is a solution
+    if (startNumberOfSatisfiedClauses == numberOfClauses) {
+        printf("s SATISFIABLE\n");
+        return 1;
+    }
+    
+    unsigned int numberOfNeighbourhoods = 100;
+    unsigned int currentNeighbourhood = 1;
+    unsigned int indexOfNeighbourhoods = 0;
+    unsigned int flips = 1;
+    unsigned int maxFlips = 100;
     unsigned int rest = 0;
     
     // maximal number of flips
@@ -58,13 +231,23 @@ int VND::search(){
     // ends if there is a solution
     while(true) {
     
+        //+++++++++++++
+        // TODO: wenn er in einem flip abbricht und noch nicht fertig mit
+        //       dem flip ist, dann flip nicht erhöhen
+        //      Note: Anzahl der Nachbarn gleich halten und nur in diesem Bereich springen
+        //      --> creatNeighbour solange aufrufen bis Grenze erreicht ist und dann Grenze erhöhen (Ausschnitt)
+        //+++++++++++++
+        
         // runs until all variables were flipped in this level or number of neighbourhoods were reached    
-        while( (currentNeighbourhood <= 20) ) {
+        while( (currentNeighbourhood <= numberOfNeighbourhoods) ) {
 
             rest = solvObject->createNeighbour(flips);
             
             if (rest == 1) {
-                solvObject->createNeighbour(flips+1);
+                flips += 1;
+                cout << "Flips: " << flips << endl;
+                solvObject->resetFlipper();
+                solvObject->createNeighbour(flips);
             } 
             
             // shift the index if maxFlip is reached
@@ -103,15 +286,30 @@ int VND::search(){
 
             // inc number of sawn neighbours
             currentNeighbourhood += 1; 
+            
+            
+            //cout << "current SAT clauses: " << newNumberOfSatisfiedClauses << endl;
+            //solvObject->printVariablesAssignment();
+            
+            // revert flip
+            solvObject->flipVariablesByFlipperVector();
         }
     
         cout << "Next best SAT Clauses: " << bestNumberOfSatisfiedClauses << endl;
         
+        while(solvObject->createNeighbour(flips) < 1) {
+        }
+        
         // no improvements with this flip
         if (startNumberOfSatisfiedClauses >= bestNumberOfSatisfiedClauses) {
+            
+            // gets the best variable combination back
+            //solvObject->useFlipperCopy(flipperCopy);
+            //solvObject->flipVariablesByFlipperVector();
+            
             flips += 1;
             solvObject->resetFlipper();
-            cout << "next flip: " << flips << endl;
+            //cout << "next flip: " << flips << endl;
         }
     
         // get the best neighbour and continue
@@ -126,10 +324,14 @@ int VND::search(){
     
         // set back for a new run
         currentNeighbourhood = 1;
+        
+        solvObject->printVariablesAssignment();
+        
     }
     
+ return 0;  
     
-    
+}   
     
     
     
@@ -236,8 +438,5 @@ int VND::search(){
     }
    
    */
-    
-return 0;  
-    
-}
+   
 
